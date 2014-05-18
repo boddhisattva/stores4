01eecc9eb41bda2c579f02f5195ef0f21c6e0ed4
Mohnish Jadwani <mail4mohnish@gmail.com>
2014-05-18 18:27:59 +0530

    adding help related details

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Stores4::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/contact"
-  get "static_pages/about"
-
+ #get "static_pages/home" the old way of defining them. We are now using REST path defined below.
+  
   root  'static_pages#home'
 
   match 'home', to: 'static_pages#home', via: 'get'
   match 'contact', to: 'static_pages#contact', via: 'get'
   match 'about', to: 'static_pages#about', via: 'get'
+  match 'help', to: 'static_pages#help', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
